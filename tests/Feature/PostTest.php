84a0ce6b132847876b4d7b807fd5746cde819110
Jennifer <jeante05@gmail.com>
2022-10-14 18:09:03 -0500

    feat: creation test update

--- tests/Feature/PostTest.php
+++ tests/Feature/PostTest.php
@@ -3,6 +3,8 @@
 namespace Tests\Feature;
 
 use App\Actions\Post\StorePostActions;
+use App\Actions\Post\UpdatePostActions;
+use App\Models\Post;
 use App\Repositories\Post\ColeccionsPostsRepositories;
 use Tests\TestCase;
 
@@ -29,7 +31,44 @@ class PostTest extends TestCase
 
     public function test_post_store_screen_can_be_rendered(): void
     {
-        $response = $this->get(route('api.posts.store'));
+        $data = [
+            "creator" => "andrea",
+            "title" => "nuevo",
+            "description" => "fasfda",
+            "photo" => "fad"
+        ];
+
+        $response = $this->post(route('api.posts.store'), $data);
+
+        $response->assertStatus(200);
+    }
+
+
+    public function test_post_actions_update(): void
+    {
+        $idPost = Post::first();
+        $data = [
+                "creator" => "Update Andrea",
+                "title" => "nuevo",
+                "description" => "fasfda",
+                "photo" => "fad"
+        ];
+
+        $create = UpdatePostActions::execute( $idPost->id, $data);
+        $this->assertSame((int)$create,  1);
+    }
+
+    public function test_post_update_screen_can_be_rendered(): void
+    {
+        $idPost = Post::first();
+        $data = [
+            "creator" => "andrea2",
+            "title" => "nuevo",
+            "description" => "fasfda",
+            "photo" => "fad"
+        ];
+
+       $response = $this->putJson('/api/posts/' .$idPost->id, $data);
 
         $response->assertStatus(200);
     }
