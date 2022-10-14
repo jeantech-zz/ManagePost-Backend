801b3d006a051e7b11c982b472b819944469b8c6
Jennifer <jeante05@gmail.com>
2022-10-14 16:22:46 -0500

    feat: create store

--- tests/Feature/PostTest.php
+++ tests/Feature/PostTest.php
@@ -3,14 +3,14 @@
 namespace Tests\Feature;
 
 use App\Actions\Post\StorePostActions;
+use App\Repositories\Post\ColeccionsPostsRepositories;
 use Tests\TestCase;
 
 class PostTest extends TestCase
 {
     public function test_post_index_screen_can_be_rendered(): void
     {
-        $response = $this->get(route('posts.index'));
-
+        $response = $this->get(route('api.posts.index'));
         $response->assertStatus(200);
     }
 
@@ -27,5 +27,11 @@ class PostTest extends TestCase
         $this->assertSame((string)$create->title,  "nuevo");
     }
 
+    public function test_post_store_screen_can_be_rendered(): void
+    {
+        $response = $this->get(route('api.posts.store'));
+
+        $response->assertStatus(200);
+    }
 }
 
