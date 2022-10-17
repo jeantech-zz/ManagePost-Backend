b28cd93ac23188411b9d48948cd38ce800b717d0
Jennifer <jeante05@gmail.com>
2022-10-17 16:48:40 -0500

    fix: update itsnot require input creator

--- app/Http/Requests/PostUpdateRequest.php
+++ app/Http/Requests/PostUpdateRequest.php
@@ -14,7 +14,6 @@ class PostUpdateRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'creator' => ['required'],
             'title' => ['required'],
             'description' => ['required'],
             'photo' => ['required'],
